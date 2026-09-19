3d4b225f7a7c754c7a60c445cd823fb7b32eb81a
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-19 12:12:30 -0300

    feat: adiciona rota para listar de transacoesController

--- src/back/routes/transacoesRoute.js
+++ src/back/routes/transacoesRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const transacoesController = require("../controllers/transacoesController");
 
 router.post("/transacoes", transacoesController.criar);
+router.get("/transacoes", transacoesController.listar);
 
 module.exports = router;
