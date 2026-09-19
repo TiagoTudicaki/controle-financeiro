ca88ea1c0f16c38639aaea784ae416713084f64f
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-19 16:52:10 -0300

    feat: adiciona rota para atualizar em transacoesController

--- src/back/routes/transacoesRoute.js
+++ src/back/routes/transacoesRoute.js
@@ -4,5 +4,6 @@ const transacoesController = require("../controllers/transacoesController");
 
 router.post("/transacoes", transacoesController.criar);
 router.get("/transacoes", transacoesController.listar);
+router.patch("/transacoes/:id", transacoesController.atualizar);
 
 module.exports = router;
