db7cc2e9d08747d8e34712054f00d36e5b966e22
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-20 17:31:40 -0300

    feat: adiciona rota de listar em transacoesController

--- src/back/routes/transacoesRoute.js
+++ src/back/routes/transacoesRoute.js
@@ -4,6 +4,7 @@ const transacoesController = require("../controllers/transacoesController");
 
 router.post("/transacoes", transacoesController.criar);
 router.get("/transacoes", transacoesController.listar);
+router.get("/transacoes", transacoesController.listar);
 router.patch("/transacoes/:id", transacoesController.atualizar);
 router.delete('/transacoes/:id', transacoesController.excluir);
 
