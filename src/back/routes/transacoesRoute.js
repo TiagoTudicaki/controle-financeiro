45e216f0126f7a089c1a2699ba113141518ada1b
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-20 15:50:12 -0300

    feat adiciona rota para função excluir de transacoesController

--- src/back/routes/transacoesRoute.js
+++ src/back/routes/transacoesRoute.js
@@ -5,5 +5,6 @@ const transacoesController = require("../controllers/transacoesController");
 router.post("/transacoes", transacoesController.criar);
 router.get("/transacoes", transacoesController.listar);
 router.patch("/transacoes/:id", transacoesController.atualizar);
+router.delete('/transacoes/:id', transacoesController.excluir);
 
 module.exports = router;
